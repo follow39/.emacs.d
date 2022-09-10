8a05f680b15a44da01bfb8444d65bfe8a2f168bc
follow39 <ingang39@gmail.com>
2022-09-10 22:51:15 +0200

    Update config

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(magit orderless flycheck yasnippet company rustic undo-tree neotree all-the-icons vterm gcmh toc-org eshell-syntax-highlighting vertico doom-modeline dashboard general projectile dap-mode exec-path-from-shell lsp-ui lsp-mode which-key doom-themes use-package evil-collection)))
+   '(ccls helpful dired-collapse dired-ranger dired-single dired-rainbow magit orderless flycheck yasnippet company rustic undo-tree neotree all-the-icons vterm gcmh toc-org eshell-syntax-highlighting vertico doom-modeline dashboard general projectile dap-mode exec-path-from-shell lsp-ui lsp-mode which-key doom-themes use-package evil-collection)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
