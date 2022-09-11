3a3d69eb15f9d6557691e104ddf8499e127c0f06
follow39 <ingang39@gmail.com>
2022-09-11 12:27:25 +0200

    Update config

--- init.el
+++ init.el
@@ -1,18 +1,4 @@
 (org-babel-load-file
  (expand-file-name
-  "config.org"
+  "new_config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
- '(package-selected-packages
-   '(ccls helpful dired-collapse dired-ranger dired-single dired-rainbow magit orderless flycheck yasnippet company rustic undo-tree neotree all-the-icons vterm gcmh toc-org eshell-syntax-highlighting vertico doom-modeline dashboard general projectile dap-mode exec-path-from-shell lsp-ui lsp-mode which-key doom-themes use-package evil-collection)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
